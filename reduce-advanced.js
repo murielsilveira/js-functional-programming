dcc74c26ec7a0a52d889ee252dee97db1123e19a
Muriel Silveira <murielkong@gmail.com>
2016-03-01 20:20:09 -0300

    Fix reduce

--- reduce-advanced.js
+++ reduce-advanced.js
@@ -5,7 +5,7 @@ var output = fs.readFileSync('data.txt', 'utf8')
 	.split('\n')
 	.map(line => line.split('\t'))
 	.reduce((customers, line) => {
-		customers[line[0]] = []
+		customers[line[0]] = customers[line[0]] || []
 		customers[line[0]].push({
 			name: line[1],
 			price: line[2],
