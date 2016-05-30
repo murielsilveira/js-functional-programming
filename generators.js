cfb5f95c21676dd6559f9809854658323b15108c
Muriel Silveira <murielkong@gmail.com>
2016-05-30 19:28:47 -0300

    in progress... going to the gym

--- generators.js
+++ generators.js
@@ -1,6 +1,20 @@
 const fetch = require('node-fetch')
+const co = require('co')
 
-fetch('http://jsonplaceholder.typicode.com/posts/1')
-  .then(r => r.json())
-  .then(post => post.title)
-  .then(title => console.log('Title:', title))
+// fetch('http://jsonplaceholder.typicode.com/posts/1')
+//   .then(r => r.json())
+//   .then(post => post.title)
+//   .then(title => console.log('Title:', title))
+
+run(function *() {
+  const uri = 'http://jsonplaceholder.typicode.com/posts/1'
+  const response = yield fetch(uri)
+  const post = yield response.json()
+  const title = post.title
+  console.log('Title:', title)
+})
+
+function run(generator) {
+  const iterator = generator()
+  console.log(iterator.next())
+}
