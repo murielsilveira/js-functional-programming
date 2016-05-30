53cbfd09abde36dde1eea5473a2aceb0e6a2ebc1
Muriel Silveira <murielkong@gmail.com>
2016-05-30 19:07:19 -0300

    Extract title

--- generators.js
+++ generators.js
@@ -1,4 +1,6 @@
 const fetch = require('node-fetch')
 
 fetch('http://jsonplaceholder.typicode.com/posts/1')
-  .then(console.log)
+  .then(r => r.json())
+  .then(post => post.title)
+  .then(title => console.log('Title:', title))
